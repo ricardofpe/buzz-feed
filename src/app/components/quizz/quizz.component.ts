50a776d313851acf16c45c12e2605d4dd68f53c7
Ricardo F Pereira <ricardofonseca_1@hotmail.com>
2024-02-19 19:54:12 -0300

    Create questions and results

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import quizzquestions from '../../../assets/data/quizz-questions.json'
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
@@ -13,6 +14,7 @@ export class QuizzComponent implements OnInit {
 
   answers:string[] = []
   answerSelected:string = ""
+  answerSelectedImg:any
 
   questionIndex:number = 0
   questionMaxIndex:number = 0
@@ -20,9 +22,72 @@ export class QuizzComponent implements OnInit {
   finished:boolean = false
 
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    if(quizzquestions){
+      this.finished = false
+      this.title = quizzquestions.title
+      this.questions = quizzquestions.questions
+      this.questionSelected = this.questions[this.questionIndex]
+
+
+
+      this.questionIndex = 0
+      this.questionMaxIndex = this.questions.length
+    }
   }
+
+  caracterChoose(value:string){
+    this.answers.push(value)
+   this.nextAnswer()
+
+  }
+
+  async nextAnswer(){
+    this.questionIndex+=1
+
+    if(this.questionMaxIndex > this.questionIndex){
+
+      this.questionSelected = this.questions[this.questionIndex]
+
+    }else{
+
+      const finalAnswer:string = await this.checkResult(this.answers)
+
+      this.finished = true
+      this.answerSelected = quizzquestions.results[finalAnswer as keyof typeof quizzquestions.results]
+      this.answerSelectedImg = quizzquestions.resultsImg[finalAnswer as keyof typeof quizzquestions.resultsImg]
+
+    }
+
+
+
+  }
+
+
+
+  async checkResult(answers:string[]){
+
+    const result = answers.reduce((previous, current, i, arr)=>{
+
+      if(arr.filter(item => item === previous).length >
+      arr.filter(item => item === current).length )
+
+      {
+
+        return previous
+
+      }else{
+
+        return current
+      }
+
+    })
+
+    return result
+
+  }
+
 
 }
