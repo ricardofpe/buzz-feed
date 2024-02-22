1b8eda88f2fa51f4a95e0f747225a0577bd2cf33
Ricardo F Pereira <ricardofonseca_1@hotmail.com>
2024-02-22 18:44:40 -0300

    Adjust responsives

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -4,7 +4,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
-  styleUrls: ['./quizz.component.css']
+  styleUrls: ['./quizz.component.css', './quizz.component.responsive.css']
 })
 export class QuizzComponent implements OnInit {
 
